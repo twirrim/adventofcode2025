c9b37fd1201ae2236f58c68fff495200b76e7c59
Paul Graydon <paulgraydon@hotmail.com>
2025-12-14 18:50:25 -0800

    Day 2, Part 2

--- src/bin/day2.rs
+++ src/bin/day2.rs
@@ -3,9 +3,11 @@ use std::ops::RangeInclusive;
 use advent_of_code_2025::*;
 
 fn parse_input(filename: &str) -> Vec<RangeInclusive<usize>> {
+    let _timer = Timer::start("Parsing input".to_owned());
     debug_println!("Reading {}", filename);
     let source = read_file(filename);
     let line = source.first().unwrap(); // Input only has a single line
+    debug_println!("Converting line into a vec of inclusive ranges");
     line.split(",") // entries are separated by commas
         .map(|x| {
             x.split("-") // each entry takes the form "start_number dash end_number", so we need to parse those digits.
@@ -21,8 +23,34 @@ fn parse_input(filename: &str) -> Vec<RangeInclusive<usize>> {
         .collect()
 }
 
+fn remove_first_and_last_letters(id: &str) -> String {
+    let mut id = id.to_string();
+    id.pop(); // remove last
+    if id.len() > 0 {
+        id.remove(0); // remove first
+    }
+    id
+}
+
+fn invalid_id_part_two(id: usize) -> bool {
+    debug_println!("Evaluating {id} using part two approach");
+    // Use the part one approach first
+    if invalid_id_part_one(id) {
+        return true;
+    }
+    // Then use the part two extension
+    // This is taking advantage of the "string doubling" technique I found searching for more optimal approaches than brute forcing it
+    // Essentially, if a string S is made of repeating patterns, then the string will exist inside S+S that has first and last letters trimmed from it
+    let doubled_id: String = format!("{id}{id}");
+    let stripped = remove_first_and_last_letters(&doubled_id);
+    if stripped.contains(&format!("{id}")) {
+        return true;
+    }
+    false
+}
+
 fn invalid_id_part_one(id: usize) -> bool {
-    debug_println!("Evaluating {id}");
+    debug_println!("Evaluating {id} using part one approach");
     let text = format!("{id}");
     let (first, second) = text.split_at(text.len() / 2);
     debug_println!("First: {first}. Second: {second}");
@@ -36,7 +64,7 @@ fn invalid_id_part_one(id: usize) -> bool {
 }
 
 fn part_one(data: &Vec<RangeInclusive<usize>>) -> usize {
-    let timer = Timer::start("Part One".to_owned());
+    let _timer = Timer::start("Part One".to_owned());
     let mut tally = 0;
     for range in data {
         debug_println!("Evaluating range\t{:?}", range);
@@ -48,26 +76,33 @@ fn part_one(data: &Vec<RangeInclusive<usize>>) -> usize {
         }
     }
     println!("Part One Result: {tally}");
-    timer.elapsed();
     tally
 }
 
 fn part_two(data: &Vec<RangeInclusive<usize>>) -> usize {
-    let timer = Timer::start("Part Two".to_owned());
+    let _timer = Timer::start("Part Two".to_owned());
     let mut tally = 0;
+    for range in data {
+        debug_println!("Evaluating range\t{:?}", range);
+        for id in range.clone().into_iter() {
+            if invalid_id_part_two(id) {
+                debug_println!("Invalid ID found: {id}");
+                tally += id;
+            }
+        }
+    }
 
     println!("Part Two Result: {tally}");
 
-    timer.elapsed();
     tally
 }
 
 fn main() {
-    let timer = Timer::start("Day Two".to_owned());
+    let _timer = Timer::start("Day Two".to_owned());
     let data = parse_input("./data/day2.txt");
     debug_println!("{:?}", data);
     part_one(&data);
-    timer.elapsed();
+    part_two(&data);
 }
 
 #[cfg(test)]
@@ -76,9 +111,15 @@ mod tests {
     use rstest::rstest;
 
     #[rstest]
-    fn test_sample_data() {
+    fn test_sample_data_part_one() {
         let data = parse_input("./data/day2_test");
         assert_eq!(part_one(&data), 1227775554);
+    }
+
+    #[rstest]
+    fn test_sample_data_part_two() {
+        let data = parse_input("./data/day2_test");
+        assert_eq!(part_two(&data), 4174379265);
     }
 
     #[rstest]
